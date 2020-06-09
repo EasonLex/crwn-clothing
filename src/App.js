dab19979e1576c2823c2c3e37f03dbde44a454c8
EasonLin <easonlin@Rende-iMac.local>
2020-06-09 14:03:29 +0800

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Header from './components/header/header.component'
@@ -19,9 +19,9 @@ class App extends React.Component {
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      console.log(userAuth)
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
             id: snapShot.id,
@@ -30,7 +30,7 @@ class App extends React.Component {
         })
       }
 
-      setCurrentUser({userAuth})
+      setCurrentUser(userAuth)
     })
   }
 
@@ -45,7 +45,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component = {HomePage} />
           <Route path='/shop' component = {ShopPage} />
-          <Route path='/signin' component = {SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
@@ -53,8 +53,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser,
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,27 +8,32 @@ import { ReactComponent as Logo }  from '../../assests/crown.svg'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
-            <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='shop'>
-                CONTACT
-            </Link>
-            {
-                currentUser?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-        </div>
-    </div>
-)
+const Header = ({ currentUser }) => {
+    console.log(currentUser)
+    return (
+        (
+            <div className='header'>
+                <Link className='logo-container' to="/">
+                    <Logo className='logo'/>
+                </Link>
+                <div className='options'>
+                    <Link className='option' to='shop'>
+                        SHOP
+                    </Link>
+                    <Link className='option' to='shop'>
+                        CONTACT
+                    </Link>
+                    {
+                        currentUser?
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className='option' to='/signin'>SIGN IN</Link>
+                    }
+                </div>
+            </div>
+        )
+    )
+}
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser,
