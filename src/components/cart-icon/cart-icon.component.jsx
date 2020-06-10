2eafef9809616592cf5662d8aef2270bdb5882a0
EasonLin <easonlin@Rende-iMac.local>
2020-06-10 09:28:08 +0800

    updating our application with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,17 +4,42 @@ import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg'
 import { connect } from'react-redux'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selector'
+
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
-    </div>
-)
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
+    console.log('render cart icon')
+    return  (
+        <div className='cart-icon' onClick={toggleCartHidden}>
+            <ShoppingIcon className='shopping-icon' />
+            <span className='item-count'>{itemCount}</span>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    console.log('need to update')
+    return (
+        {
+            itemCount: selectCartItemsCount(state),
+        }
+    )
+}
+
+// const mapStateToProps = ({cart: {cartItems}}) => {
+//     console.log('need to update')
+//     return ({
+//         itemCount: cartItems.reduce(
+//             (accumlatedQuantity, carItem) => 
+//                 accumlatedQuantity + carItem.quantity, 
+//             0
+//         )
+//     })
+// }
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
